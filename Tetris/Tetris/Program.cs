46aea91df0b838794a081d271f398a1b565e09ec
Helge <Helge@SURFACE3PRO>
2017-06-29 11:21:14 +0200

    Umbenennung und Extraktion von Methoden

--- Tetris/Tetris/Program.cs
+++ Tetris/Tetris/Program.cs
@@ -24,27 +24,32 @@ namespace Tetris
 
         static void Main()
         {
-            Console.OutputEncoding = System.Text.Encoding.UTF8;
-            drawBorder();
-            Console.SetCursorPosition(4, 5);
-            Console.WriteLine("Press any key");
-            Console.ReadKey(true);
-            timer.Start();
-            dropTimer.Start();
-            long time = timer.ElapsedMilliseconds;
-            Console.SetCursorPosition(25, 0);
-            Console.WriteLine("Level " + level);
-            Console.SetCursorPosition(25, 1);
-            Console.WriteLine("Score " + score);
-            Console.SetCursorPosition(25, 2);
-            Console.WriteLine("LinesCleared " + linesCleared);
-            nexttet = new Tetrominoe();
-            tet = nexttet;
-            tet.Spawn();
-            nexttet = new Tetrominoe();
+            bool stayInLoop = true;
 
-            Update();
+            while (stayInLoop)
+            {
+                Console.OutputEncoding = System.Text.Encoding.UTF8;
 
+                SetupUI();
+
+                WaitForStart();
+
+                SetupTimer();
+
+                nexttet = new Tetrominoe();
+                tet = nexttet;
+                tet.Spawn();
+                nexttet = new Tetrominoe();
+
+                Update();
+
+                stayInLoop = WaitForRestartOrCancel();
+            }
+
+        }
+
+        private static bool WaitForRestartOrCancel()
+        {
             Console.SetCursorPosition(0, 0);
             Console.WriteLine("Game Over \n Replay? (Y/N)");
             string input = Console.ReadLine();
@@ -64,10 +69,35 @@ namespace Tetris
                 level = 1;
                 GC.Collect();
                 Console.Clear();
-                Main();
             }
-            else return;
 
+            else return true;
+            return false;
+        }
+
+        private static void SetupTimer()
+        {
+            timer.Start();
+            dropTimer.Start();
+            long time = timer.ElapsedMilliseconds;
+        }
+
+        private static void WaitForStart()
+        {
+            Console.SetCursorPosition(4, 5);
+            Console.WriteLine("Press any key");
+            Console.ReadKey(true);
+        }
+
+        private static void SetupUI()
+        {
+            drawBorder();
+            Console.SetCursorPosition(25, 0);
+            Console.WriteLine("Level " + level);
+            Console.SetCursorPosition(25, 1);
+            Console.WriteLine("Score " + score);
+            Console.SetCursorPosition(25, 2);
+            Console.WriteLine("LinesCleared " + linesCleared);
         }
 
         private static void fillGrid()
@@ -269,387 +299,6 @@ namespace Tetris
                 Console.Write("*-");
             }
 
-        }
-
-    }
-
-    public class Tetrominoe
-    {
-        public static int[,] I = new int[1, 4] { { 1, 1, 1, 1 } };//3
-        public static int[,] O = new int[2, 2] { { 1, 1 }, { 1, 1 } };
-        public static int[,] T = new int[2, 3] { { 0, 1, 0 }, { 1, 1, 1 } };//3
-        public static int[,] S = new int[2, 3] { { 0, 1, 1 }, { 1, 1, 0 } };//4
-        public static int[,] Z = new int[2, 3] { { 1, 1, 0 }, { 0, 1, 1 } };//3
-        public static int[,] J = new int[2, 3] { { 1, 0, 0 }, { 1, 1, 1 } };//3
-        public static int[,] L = new int[2, 3] { { 0, 0, 1 }, { 1, 1, 1 } };//3
-        public static List<int[,]> tetrominoes = new List<int[,]>() { I, O, T, S, Z, J, L };
-
-        private bool isErect = false;
-        private int[,] shape;
-        private int[] pix = new int[2];
-        public List<int[]> location = new List<int[]>();
-
-        public Tetrominoe()
-        {
-            Random rnd = new Random();
-            shape = tetrominoes[rnd.Next(0, 7)];
-            for (int i = 23; i < 33; ++i)
-            {
-                for (int j = 3; j < 10; j++)
-                {
-                    Console.SetCursorPosition(i, j);
-                    Console.Write("  ");
-                }
-
-            }
-            Program.drawBorder();
-            for (int i = 0; i < shape.GetLength(0); i++)
-            {
-                for (int j = 0; j < shape.GetLength(1); j++)
-                {
-                    if (shape[i, j] == 1)
-                    {
-                        Console.SetCursorPosition(((10 - shape.GetLength(1)) / 2 + j) * 2 + 20, i + 5);
-                        Console.Write(Program.sqr);
-                    }
-                }
-            }
-        }
-
-        public void Spawn()
-        {
-            for (int i = 0; i < shape.GetLength(0); i++)
-            {
-                for (int j = 0; j < shape.GetLength(1); j++)
-                {
-                    if (shape[i, j] == 1)
-                    {
-                        location.Add(new int[] { i, (10 - shape.GetLength(1)) / 2 + j });
-                    }
-                }
-            }
-            Update();
-        }
-
-        public void Drop()
-        {
-
-            if (isSomethingBelow())
-            {
-                for (int i = 0; i < 4; i++)
-                {
-                    Program.droppedtetrominoeLocationGrid[location[i][0], location[i][1]] = 1;
-                }
-                Program.isDropped = true;
-
-            }
-            else
-            {
-                for (int numCount = 0; numCount < 4; numCount++)
-                {
-                    location[numCount][0] += 1;
-                }
-                Update();
-            }
-        }
-
-        public void Rotate()
-        {
-            List<int[]> templocation = new List<int[]>();
-            for (int i = 0; i < shape.GetLength(0); i++)
-            {
-                for (int j = 0; j < shape.GetLength(1); j++)
-                {
-                    if (shape[i, j] == 1)
-                    {
-                        templocation.Add(new int[] { i, (10 - shape.GetLength(1)) / 2 + j });
-                    }
-                }
-            }
-
-            if (shape == tetrominoes[0])
-            {
-                if (isErect == false)
-                {
-                    for (int i = 0; i < location.Count; i++)
-                    {
-                        templocation[i] = TransformMatrix(location[i], location[2], "Clockwise");
-                    }
-                }
-                else
-                {
-                    for (int i = 0; i < location.Count; i++)
-                    {
-                        templocation[i] = TransformMatrix(location[i], location[2], "Counterclockwise");
-                    }
-                }
-            }
-
-            else if (shape == tetrominoes[3])
-            {
-                for (int i = 0; i < location.Count; i++)
-                {
-                    templocation[i] = TransformMatrix(location[i], location[3], "Clockwise");
-                }
-            }
-
-            else if (shape == tetrominoes[1]) return;
-            else
-            {
-                for (int i = 0; i < location.Count; i++)
-                {
-                    templocation[i] = TransformMatrix(location[i], location[2], "Clockwise");
-                }
-            }
-
-
-            for (int count = 0; isOverlayLeft(templocation) != false | isOverlayRight(templocation) != false | isOverlayBelow(templocation) != false; count++)
-            {
-                if (isOverlayLeft(templocation) == true)
-                {
-                    for (int i = 0; i < location.Count; i++)
-                    {
-                        templocation[i][1] += 1;
-                    }
-                }
-
-                if (isOverlayRight(templocation) == true)
-                {
-                    for (int i = 0; i < location.Count; i++)
-                    {
-                        templocation[i][1] -= 1;
-                    }
-                }
-                if (isOverlayBelow(templocation) == true)
-                {
-                    for (int i = 0; i < location.Count; i++)
-                    {
-                        templocation[i][0] -= 1;
-                    }
-                }
-                if (count == 3)
-                {
-                    return;
-                }
-            }
-
-            location = templocation;
-
-        }
-
-        public int[] TransformMatrix(int[] coord, int[] axis, string dir)
-        {
-            int[] pcoord = { coord[0] - axis[0], coord[1] - axis[1] };
-            if (dir == "Counterclockwise")
-            {
-                pcoord = new int[] { -pcoord[1], pcoord[0] };
-            }
-            else if (dir == "Clockwise")
-            {
-                pcoord = new int[] { pcoord[1], -pcoord[0] };
-            }
-
-            return new int[] { pcoord[0] + axis[0], pcoord[1] + axis[1] };
-        }
-
-        public bool isSomethingBelow()
-        {
-            for (int i = 0; i < 4; i++)
-            {
-                if (location[i][0] + 1 >= 23)
-                    return true;
-                if (location[i][0] + 1 < 23)
-                {
-                    if (Program.droppedtetrominoeLocationGrid[location[i][0] + 1, location[i][1]] == 1)
-                    {
-                        return true;
-                    }
-                }
-            }
-            return false;
-        }
-        public bool? isOverlayBelow(List<int[]> location)
-        {
-            List<int> ycoords = new List<int>();
-            for (int i = 0; i < 4; i++)
-            {
-                ycoords.Add(location[i][0]);
-                if (location[i][0] >= 23)
-                    return true;
-                if (location[i][0] < 0)
-                    return null;
-                if (location[i][1] < 0)
-                {
-                    return null;
-                }
-                if (location[i][1] > 9)
-                {
-                    return null;
-                }
-            }
-            for (int i = 0; i < 4; i++)
-            {
-                if (ycoords.Max() - ycoords.Min() == 3)
-                {
-                    if (ycoords.Max() == location[i][0] | ycoords.Max() - 1 == location[i][0])
-                    {
-                        if (Program.droppedtetrominoeLocationGrid[location[i][0], location[i][1]] == 1)
-                        {
-                            return true;
-                        }
-                    }
-
-                }
-                else
-                {
-                    if (ycoords.Max() == location[i][0])
-                    {
-                        if (Program.droppedtetrominoeLocationGrid[location[i][0], location[i][1]] == 1)
-                        {
-                            return true;
-                        }
-                    }
-                }
-            }
-
-            return false;
-        }
-
-
-        public bool isSomethingLeft()
-        {
-            for (int i = 0; i < 4; i++)
-            {
-                if (location[i][1] == 0)
-                {
-                    return true;
-                }
-                else if (Program.droppedtetrominoeLocationGrid[location[i][0], location[i][1] - 1] == 1)
-                {
-                    return true;
-                }
-            }
-            return false;
-        }
-        public bool? isOverlayLeft(List<int[]> location)
-        {
-            List<int> xcoords = new List<int>();
-            for (int i = 0; i < 4; i++)
-            {
-                xcoords.Add(location[i][1]);
-                if (location[i][1] < 0)
-                {
-                    return true;
-                }
-                if (location[i][1] > 9)
-                {
-                    return false;
-                }
-                if (location[i][0] >= 23)
-                    return null;
-                if (location[i][0] < 0)
-                    return null;
-            }
-            for (int i = 0; i < 4; i++)
-            {
-                if (xcoords.Max() - xcoords.Min() == 3)
-                {
-                    if (xcoords.Min() == location[i][1] | xcoords.Min() + 1 == location[i][1])
-                    {
-                        if (Program.droppedtetrominoeLocationGrid[location[i][0], location[i][1]] == 1)
-                        {
-                            return true;
-                        }
-                    }
-
-                }
-                else
-                {
-                    if (xcoords.Min() == location[i][1])
-                    {
-                        if (Program.droppedtetrominoeLocationGrid[location[i][0], location[i][1]] == 1)
-                        {
-                            return true;
-                        }
-                    }
-                }
-            }
-            return false;
-        }
-        public bool isSomethingRight()
-        {
-            for (int i = 0; i < 4; i++)
-            {
-                if (location[i][1] == 9)
-                {
-                    return true;
-                }
-                else if (Program.droppedtetrominoeLocationGrid[location[i][0], location[i][1] + 1] == 1)
-                {
-                    return true;
-                }
-            }
-            return false;
-        }
-        public bool? isOverlayRight(List<int[]> location)
-        {
-            List<int> xcoords = new List<int>();
-            for (int i = 0; i < 4; i++)
-            {
-                xcoords.Add(location[i][1]);
-                if (location[i][1] > 9)
-                {
-                    return true;
-                }
-                if (location[i][1] < 0)
-                {
-                    return false;
-                }
-                if (location[i][0] >= 23)
-                    return null;
-                if (location[i][0] < 0)
-                    return null;
-            }
-            for (int i = 0; i < 4; i++)
-            {
-                if (xcoords.Max() - xcoords.Min() == 3)
-                {
-                    if (xcoords.Max() == location[i][1] | xcoords.Max() - 1 == location[i][1])
-                    {
-                        if (Program.droppedtetrominoeLocationGrid[location[i][0], location[i][1]] == 1)
-                        {
-                            return true;
-                        }
-                    }
-
-                }
-                else
-                {
-                    if (xcoords.Max() == location[i][1])
-                    {
-                        if (Program.droppedtetrominoeLocationGrid[location[i][0], location[i][1]] == 1)
-                        {
-                            return true;
-                        }
-                    }
-                }
-            }
-            return false;
-        }
-        public void Update()
-        {
-            for (int i = 0; i < 23; i++)
-            {
-                for (int j = 0; j < 10; j++)
-                {
-                    Program.grid[i, j] = 0;
-                }
-            }
-            for (int i = 0; i < 4; i++)
-            {
-                Program.grid[location[i][0], location[i][1]] = 1;
-            }
-            Program.Draw();
         }
     }
 }
---
+++ Tetris/Tetris/Tetrominoe.cs
@@ -0,0 +1,388 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tetris
+{
+    public class Tetrominoe
+    {
+        public static int[,] I = new int[1, 4] { { 1, 1, 1, 1 } };//3
+        public static int[,] O = new int[2, 2] { { 1, 1 }, { 1, 1 } };
+        public static int[,] T = new int[2, 3] { { 0, 1, 0 }, { 1, 1, 1 } };//3
+        public static int[,] S = new int[2, 3] { { 0, 1, 1 }, { 1, 1, 0 } };//4
+        public static int[,] Z = new int[2, 3] { { 1, 1, 0 }, { 0, 1, 1 } };//3
+        public static int[,] J = new int[2, 3] { { 1, 0, 0 }, { 1, 1, 1 } };//3
+        public static int[,] L = new int[2, 3] { { 0, 0, 1 }, { 1, 1, 1 } };//3
+        public static List<int[,]> tetrominoes = new List<int[,]>() { I, O, T, S, Z, J, L };
+
+        private bool isErect = false;
+        private int[,] shape;
+        private int[] pix = new int[2];
+        public List<int[]> location = new List<int[]>();
+
+        public Tetrominoe()
+        {
+            Random rnd = new Random();
+            shape = tetrominoes[rnd.Next(0, 7)];
+            for (int i = 23; i < 33; ++i)
+            {
+                for (int j = 3; j < 10; j++)
+                {
+                    Console.SetCursorPosition(i, j);
+                    Console.Write("  ");
+                }
+
+            }
+            Program.drawBorder();
+            for (int i = 0; i < shape.GetLength(0); i++)
+            {
+                for (int j = 0; j < shape.GetLength(1); j++)
+                {
+                    if (shape[i, j] == 1)
+                    {
+                        Console.SetCursorPosition(((10 - shape.GetLength(1)) / 2 + j) * 2 + 20, i + 5);
+                        Console.Write(Program.sqr);
+                    }
+                }
+            }
+        }
+
+        public void Spawn()
+        {
+            for (int i = 0; i < shape.GetLength(0); i++)
+            {
+                for (int j = 0; j < shape.GetLength(1); j++)
+                {
+                    if (shape[i, j] == 1)
+                    {
+                        location.Add(new int[] { i, (10 - shape.GetLength(1)) / 2 + j });
+                    }
+                }
+            }
+            Update();
+        }
+
+        public void Drop()
+        {
+
+            if (isSomethingBelow())
+            {
+                for (int i = 0; i < 4; i++)
+                {
+                    Program.droppedtetrominoeLocationGrid[location[i][0], location[i][1]] = 1;
+                }
+                Program.isDropped = true;
+
+            }
+            else
+            {
+                for (int numCount = 0; numCount < 4; numCount++)
+                {
+                    location[numCount][0] += 1;
+                }
+                Update();
+            }
+        }
+
+        public void Rotate()
+        {
+            List<int[]> templocation = new List<int[]>();
+            for (int i = 0; i < shape.GetLength(0); i++)
+            {
+                for (int j = 0; j < shape.GetLength(1); j++)
+                {
+                    if (shape[i, j] == 1)
+                    {
+                        templocation.Add(new int[] { i, (10 - shape.GetLength(1)) / 2 + j });
+                    }
+                }
+            }
+
+            if (shape == tetrominoes[0])
+            {
+                if (isErect == false)
+                {
+                    for (int i = 0; i < location.Count; i++)
+                    {
+                        templocation[i] = TransformMatrix(location[i], location[2], "Clockwise");
+                    }
+                }
+                else
+                {
+                    for (int i = 0; i < location.Count; i++)
+                    {
+                        templocation[i] = TransformMatrix(location[i], location[2], "Counterclockwise");
+                    }
+                }
+            }
+
+            else if (shape == tetrominoes[3])
+            {
+                for (int i = 0; i < location.Count; i++)
+                {
+                    templocation[i] = TransformMatrix(location[i], location[3], "Clockwise");
+                }
+            }
+
+            else if (shape == tetrominoes[1]) return;
+            else
+            {
+                for (int i = 0; i < location.Count; i++)
+                {
+                    templocation[i] = TransformMatrix(location[i], location[2], "Clockwise");
+                }
+            }
+
+
+            for (int count = 0; isOverlayLeft(templocation) != false | isOverlayRight(templocation) != false | isOverlayBelow(templocation) != false; count++)
+            {
+                if (isOverlayLeft(templocation) == true)
+                {
+                    for (int i = 0; i < location.Count; i++)
+                    {
+                        templocation[i][1] += 1;
+                    }
+                }
+
+                if (isOverlayRight(templocation) == true)
+                {
+                    for (int i = 0; i < location.Count; i++)
+                    {
+                        templocation[i][1] -= 1;
+                    }
+                }
+                if (isOverlayBelow(templocation) == true)
+                {
+                    for (int i = 0; i < location.Count; i++)
+                    {
+                        templocation[i][0] -= 1;
+                    }
+                }
+                if (count == 3)
+                {
+                    return;
+                }
+            }
+
+            location = templocation;
+
+        }
+
+        public int[] TransformMatrix(int[] coord, int[] axis, string dir)
+        {
+            int[] pcoord = { coord[0] - axis[0], coord[1] - axis[1] };
+            if (dir == "Counterclockwise")
+            {
+                pcoord = new int[] { -pcoord[1], pcoord[0] };
+            }
+            else if (dir == "Clockwise")
+            {
+                pcoord = new int[] { pcoord[1], -pcoord[0] };
+            }
+
+            return new int[] { pcoord[0] + axis[0], pcoord[1] + axis[1] };
+        }
+
+        public bool isSomethingBelow()
+        {
+            for (int i = 0; i < 4; i++)
+            {
+                if (location[i][0] + 1 >= 23)
+                    return true;
+                if (location[i][0] + 1 < 23)
+                {
+                    if (Program.droppedtetrominoeLocationGrid[location[i][0] + 1, location[i][1]] == 1)
+                    {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+        public bool? isOverlayBelow(List<int[]> location)
+        {
+            List<int> ycoords = new List<int>();
+            for (int i = 0; i < 4; i++)
+            {
+                ycoords.Add(location[i][0]);
+                if (location[i][0] >= 23)
+                    return true;
+                if (location[i][0] < 0)
+                    return null;
+                if (location[i][1] < 0)
+                {
+                    return null;
+                }
+                if (location[i][1] > 9)
+                {
+                    return null;
+                }
+            }
+            for (int i = 0; i < 4; i++)
+            {
+                if (ycoords.Max() - ycoords.Min() == 3)
+                {
+                    if (ycoords.Max() == location[i][0] | ycoords.Max() - 1 == location[i][0])
+                    {
+                        if (Program.droppedtetrominoeLocationGrid[location[i][0], location[i][1]] == 1)
+                        {
+                            return true;
+                        }
+                    }
+
+                }
+                else
+                {
+                    if (ycoords.Max() == location[i][0])
+                    {
+                        if (Program.droppedtetrominoeLocationGrid[location[i][0], location[i][1]] == 1)
+                        {
+                            return true;
+                        }
+                    }
+                }
+            }
+
+            return false;
+        }
+
+
+        public bool isSomethingLeft()
+        {
+            for (int i = 0; i < 4; i++)
+            {
+                if (location[i][1] == 0)
+                {
+                    return true;
+                }
+                else if (Program.droppedtetrominoeLocationGrid[location[i][0], location[i][1] - 1] == 1)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+        public bool? isOverlayLeft(List<int[]> location)
+        {
+            List<int> xcoords = new List<int>();
+            for (int i = 0; i < 4; i++)
+            {
+                xcoords.Add(location[i][1]);
+                if (location[i][1] < 0)
+                {
+                    return true;
+                }
+                if (location[i][1] > 9)
+                {
+                    return false;
+                }
+                if (location[i][0] >= 23)
+                    return null;
+                if (location[i][0] < 0)
+                    return null;
+            }
+            for (int i = 0; i < 4; i++)
+            {
+                if (xcoords.Max() - xcoords.Min() == 3)
+                {
+                    if (xcoords.Min() == location[i][1] | xcoords.Min() + 1 == location[i][1])
+                    {
+                        if (Program.droppedtetrominoeLocationGrid[location[i][0], location[i][1]] == 1)
+                        {
+                            return true;
+                        }
+                    }
+
+                }
+                else
+                {
+                    if (xcoords.Min() == location[i][1])
+                    {
+                        if (Program.droppedtetrominoeLocationGrid[location[i][0], location[i][1]] == 1)
+                        {
+                            return true;
+                        }
+                    }
+                }
+            }
+            return false;
+        }
+        public bool isSomethingRight()
+        {
+            for (int i = 0; i < 4; i++)
+            {
+                if (location[i][1] == 9)
+                {
+                    return true;
+                }
+                else if (Program.droppedtetrominoeLocationGrid[location[i][0], location[i][1] + 1] == 1)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+        public bool? isOverlayRight(List<int[]> location)
+        {
+            List<int> xcoords = new List<int>();
+            for (int i = 0; i < 4; i++)
+            {
+                xcoords.Add(location[i][1]);
+                if (location[i][1] > 9)
+                {
+                    return true;
+                }
+                if (location[i][1] < 0)
+                {
+                    return false;
+                }
+                if (location[i][0] >= 23)
+                    return null;
+                if (location[i][0] < 0)
+                    return null;
+            }
+            for (int i = 0; i < 4; i++)
+            {
+                if (xcoords.Max() - xcoords.Min() == 3)
+                {
+                    if (xcoords.Max() == location[i][1] | xcoords.Max() - 1 == location[i][1])
+                    {
+                        if (Program.droppedtetrominoeLocationGrid[location[i][0], location[i][1]] == 1)
+                        {
+                            return true;
+                        }
+                    }
+
+                }
+                else
+                {
+                    if (xcoords.Max() == location[i][1])
+                    {
+                        if (Program.droppedtetrominoeLocationGrid[location[i][0], location[i][1]] == 1)
+                        {
+                            return true;
+                        }
+                    }
+                }
+            }
+            return false;
+        }
+        public void Update()
+        {
+            for (int i = 0; i < 23; i++)
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                    Program.grid[i, j] = 0;
+                }
+            }
+            for (int i = 0; i < 4; i++)
+            {
+                Program.grid[location[i][0], location[i][1]] = 1;
+            }
+            Program.Draw();
+        }
+    }
+}
